36ca534887c45e8921d014c0a31aa73e830765ad
Oden-7073 <you6857@gmail.com>
2025-01-12 23:52:06 +0900

    0112-RWD

--- work/src/components/Header.jsx
+++ work/src/components/Header.jsx
@@ -7,13 +7,12 @@ function Header() {
             <div className="l-header-left">
                 <div className="l-header-left-profile">
                     <div className="l-header-left-profile_icon">
-                        <h2>Profile</h2>
+                        <h2>Oden</h2>
                         <img src="./assets/index_fig02.webp" alt="profile icon01">
                         </img>                   
                     </div>                    
                 </div>
                 <div className="l-header-left-txtarea">
-                    <h2>Oden</h2>
                     <p className='mb_10'>From Taiwan, go to  Japan in 2019.<br/>2009 百年來的凝視ー 大学生ポスターデザインコンテスト 「佳作」賞をもらいました。</p>
                     <p>本サイトをReactで作成します。</p>
                 </div>                
